fb38b55c6b14b1771e3992976a238571d00e87e0
Valerii Vasin <valerii.vasin@gmail.com>
2015-03-23 12:13:36 +0100

    Add vagrant up script.

--- system/in.sh
+++ system/in.sh
@@ -1,16 +1,23 @@
 function IN() {
   case "$1" in
         ant)
-            _in_ant
+            in_ant
+            ;;
+        vagrunt_up)
+            in_vagrant_up;
             ;;
         *)
-            echo "Usage: IN {ant}"
+            echo "Usage: IN {ant|vagrant_up}"
             ;;
     esac
 }
 
-function _in_ant() {
+function in_ant() {
   vagrant ssh -c "cd /code/in && sudo ant; cd /code/in-solr && sudo ant;";
+}
+
+in_vagrant_up() {
+  sudo node $HOME/dotfiles/scripts/in;
 }
 
 export -f IN;
