1409c306b2793685f0c77f21888e1548405b3db1
Valerii Vasin <valerii.vasin@gmail.com>
2015-03-25 10:22:37 +0100

    Add in_routes.

--- system/in.sh
+++ system/in.sh
@@ -6,18 +6,25 @@ function IN() {
         vagrunt_up)
             in_vagrant_up;
             ;;
+        routes)
+            in_routes;
+            ;;
         *)
-            echo "Usage: IN {ant|vagrant_up}"
+            echo "Usage: IN {ant|vagrant_up|routes}"
             ;;
     esac
 }
 
-function in_ant() {
+in_ant() {
   vagrant ssh -c "cd /code/in && sudo ant; cd /code/in-solr && sudo ant;";
 }
 
 in_vagrant_up() {
   sudo node $HOME/dotfiles/scripts/in;
+}
+
+in_routes() {
+  vagrant ssh -c "cd /code/in && sudo php app-new/console router:debug"
 }
 
 export -f IN;
