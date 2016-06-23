36262799af34d184e85db9f8cd9f053ab6bde522
Valerii Vasin <valerii.vasin@gmail.com>
2016-06-23 10:24:46 +0200

    Add meta info about vscode

---
+++ init/vscode.bash
@@ -0,0 +1,4 @@
+# Remove local ~/.vscode
+# Make link <Dropbox>/Shared/.vscode => ~/.vscode
+# rm ~/Library/Application\ Support/Code/User
+# Make Link <Dropbox>/Shared/VSCodeUser to $HOME/Library/Application\ Support/Code/User
